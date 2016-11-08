2e4bea885c0f615f19d493bd75e53b46b39a93e8
devinlukianto <devindevdog@gmail.com>
2016-11-09 03:15:17 +0700

    q_get update

--- dcomm.h
+++ dcomm.h
@@ -1,32 +1,32 @@
-/* 
- * File : dcomm.h 
- */ 
+/*
+ * File : dcomm.h
+ */
 
-#ifndef _DCOMM_H_ 
-#define _DCOMM_H_ 
+#ifndef _DCOMM_H_
+#define _DCOMM_H_
 
-/* ASCII Const */ 
+/* ASCII Const */
 #define SOH 1 /* Start of Header Character */
 #define STX 2 /* Start of Text Character */
-#define ETX 3 /* End of Text Character */ 
-#define ENQ 5 /* Enquiry Character */ 
-#define ACK 6 /* Acknowledgement */ 
-#define BEL 7 /* Message Error Warning */ 
+#define ETX 3 /* End of Text Character */
+#define ENQ 5 /* Enquiry Character */
+#define ACK 6 /* Acknowledgement */
+#define BEL 7 /* Message Error Warning */
 #define CR 13 /* Carriage Return */
-#define LF 10 /* Line Feed */ 
+#define LF 10 /* Line Feed */
 #define NAK 21 /* Negative Acknowledgement */
 #define Endfile 26 /* End of file character */
-#define ESC 27 /* ESC key */ 
+#define ESC 27 /* ESC key */
 
-/* XON/XOFF protocol */ 
-#define XON (0x11) 
-#define XOFF (0x13) 
+/* XON/XOFF protocol */
+#define XON (0x11)
+#define XOFF (0x13)
 
-/* Const */ 
+/* Const */
 #define BYTESIZE 256 /* The maximum value of a byte */
-#define MAXLEN 1024 /* Maximum messages length */ 
+#define MAXLEN 1024 /* Maximum messages length */
 
-typedef enum { false=0, true } Boolean;
+typedef enum {false=0, true} Boolean;
 
 typedef unsigned char Byte;
 
